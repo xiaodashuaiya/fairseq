b0bbab21a4cc4cac10009f7729750e6aa9bfb7ed
xiaodashuai <1183143645@qq.com>
2020-09-10 22:25:02 +0800

    commit

--- 1.py
+++ 1.py
@@ -6,3 +6,12 @@ kdim = kdim if kdim is not None else edim
 print('kime')
 print('kdim=',kdim)
 # print('qkv_dim=',qkv_same_dim)
+ """
+    Transformer encoder consisting of *args.encoder_layers* layers. Each layer
+    is a :class:`TransformerEncoderLayer`.
+
+    Args:
+        args (argparse.Namespace): parsed command-line arguments
+        dictionary (~fairseq.data.Dictionary): encoding dictionary
+        embed_tokens (torch.nn.Embedding): input embedding
+    """
